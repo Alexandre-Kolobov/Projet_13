c71d3ead02a99dc9a1d6cb8cdd64696bf104b74d
Alexandre Kolobov <kolobovalexandre@gmail.com>
2023-11-24 17:20:39 +0100

    before doing migration

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,12 +3,13 @@ from django.urls import path
 
 from . import views
 from lettings.views import lettings_index, letting
+from profiles.views import profiles_index, profile
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', lettings_index, name='lettings_index'),
     path('lettings/<int:letting_id>/', letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('profiles/', profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
