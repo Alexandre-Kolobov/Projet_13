b0a23e06ff61fa55f62a12766444136bce25a993
Alexandre Kolobov <kolobovalexandre@gmail.com>
2024-01-10 10:45:37 +0100

    test demo

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', default="Test")
 # SECURITY WARNING: don't run with debug turned on in production!
-debug_env = os.environ.get('DEBUG', default="True")
+debug_env = os.environ.get('DEBUG', default="False")
 if debug_env == "False":
     DEBUG = False
 else:
